3178eefd0089e382c2a45d8e99ad526d8cd66925
Super-mehdi <Amgharyelmehdi@gmail.com>
2026-07-07 18:16:44 +0100

    define db config file

--- backend/database.py
+++ backend/database.py
@@ -1,2 +1,15 @@
-class Base:
-    pass
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / ".env")
+
+DB_URL=f"postgresql://{os.getenv("DATABASE_URL")}"
+
+engine = create_engine(DB_URL)
+session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
