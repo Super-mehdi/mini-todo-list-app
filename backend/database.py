0ec91cf35ea02a4e1fec3fae011665627c52c891
Super-mehdi <Amgharyelmehdi@gmail.com>
2026-07-07 18:42:39 +0100

    test backend and db connection

--- backend/database.py
+++ backend/database.py
@@ -2,12 +2,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 import os
-from pathlib import Path
-from dotenv import load_dotenv
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-load_dotenv(BASE_DIR / ".env")
 
 DB_URL=f"{os.getenv("DATABASE_URL")}"
 
--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from sqlalchemy import text
+
+from database import session
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def get_db():
+    db = session()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/test-db")
+def test_db(db: Session = Depends(get_db)):
+    result = db.execute(text("SELECT 1"))
+    return {"database": result.scalar() == 1}
