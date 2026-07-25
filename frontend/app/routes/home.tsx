2dd31233a9ee0d2806edc3e564b57dd269295757
Super-mehdi <Amgharyelmehdi@gmail.com>
2026-07-25 22:50:41 +0100

    clean and set up frontend folder

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,13 +1,12 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Hello  from "../components/hello";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Medo" }
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <Hello />;
 }
